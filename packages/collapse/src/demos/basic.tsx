1e2603f490511c56d2740f17ba3c22ba0d63a989
yly24 <yinqin2015@gmail.com>
2021-09-12 13:21:36 +0800

    style

--- packages/collapse/src/demos/basic.tsx
+++ packages/collapse/src/demos/basic.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import YCollapse, { Panel } from 'y-collapse';
+import Collapse, { Panel } from 'y-collapse';
 import '../assets/index.less';
 import collapseMotion from './_util/motionUtil'
 
 const YCollapseDemo = () => {
   return (
-    <YCollapse style={{ margin: 20 }} openMotion={collapseMotion}>
+    <Collapse style={{ margin: 20 }} openMotion={collapseMotion}>
       <Panel header="title">content</Panel>
       <Panel header="title">content</Panel>
       <Panel header="title">content</Panel>
-    </YCollapse>
+    </Collapse>
   );
 };
 
